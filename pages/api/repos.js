ea7c0772baec8c2f3fc8a20716ca35b175cf28bb
Türker <63150613+turkerpw@users.noreply.github.com>
2022-03-04 20:14:04 +0300

    🔨

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -1,7 +1,11 @@
 const fetch = require('isomorphic-unfetch')
 
 export default async (req, res) => {
-    let _ = await (await fetch('https://api.github.com/users/PonyXDev/repos', {
+    let _ = await (await fetch('https://api.github.com/users/turkerpw/repos', {
+        headers: {}
+    })).json();
+
+    let __ = await (await fetch('https://api.github.com/users/turkerpw/repos', {
         headers: {}
     })).json();
     try {
